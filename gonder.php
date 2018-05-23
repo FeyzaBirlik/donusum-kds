cbbb4d2928be8a63289a50e42af934f995f8b921
fyzbrlk <feyzabrlkybs@gmail.com>
2018-05-24 01:11:10 +0300

    Create gonder.php

---
+++ gonder.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>KDS</title>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+ 
+
+<style>
+
+
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+</style>
+
+
+</head>
+
+
+
+
+<div class="container">
+	<div class="row" style="    background-color: lightblue">
+
+		<section class="content">
+			<h1>YIKIM RAPORU</h1>
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						
+							<p>RAPOR BAŞARIYLA GÖNDERİLDİ</p>
+						</div>
+						
+				<div class="content-footer" style="heigth:200px; width:300px;">
+					
+						
+					
+				</div>
+			</div>
+		</section>
+		
+	</div>
+</div>
+</html>
